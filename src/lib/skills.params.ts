162d8ea45c7e45e80b94d7398a8a94289d258ef7
Luis Canada <canadaluis5@gmail.com>
2024-02-04 18:32:07 -0500

    Update skills

--- src/lib/skills.params.ts
+++ src/lib/skills.params.ts
@@ -1,71 +1,188 @@
-import Assets from './data/assets';
-import type { Skill } from './types';
-import svelte from './md/svelte.md?raw';
-
-const s = (skill: Skill) => skill;
+import Assets from "./data/assets";
+import type { Skill } from "./types";
 
 export type ArrayElementType<ArrayType extends readonly unknown[]> =
-	ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
-const MY_SKILLS = [
-	s({
-		slug: 'js',
-		color: 'yellow',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.JavaScript,
-		name: 'Javascript'
-	}),
-	s({
-		slug: 'ts',
-		color: 'blue',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.TypeScript,
-		name: 'Typescript'
-	}),
-	s({
-		slug: 'css',
-		color: 'blue',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.CSS,
-		name: 'CSS'
-	}),
-	s({
-		slug: 'html',
-		color: 'orange',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.HTML,
-		name: 'HTML'
-	}),
-	s({
-		slug: 'sass',
-		color: 'pink',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.Sass,
-		name: 'Sass'
-	}),
-	s({
-		slug: 'reactjs',
-		color: 'cyan',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.ReactJs,
-		name: 'React Js'
-	}),
-	s({
-		slug: 'svelte',
-		color: 'orange',
-		description: svelte,
-		logo: Assets.Svelte,
-		name: 'Svelte'
-	})
+const MY_SKILLS: Skill[] = [
+  {
+    slug: "js",
+    color: "yellow",
+    description: `3 years experience with Javascript
+
+		Very comfortable using in both older server side frameworks like Django and newer ones like Next JS
+		`,
+    logo: Assets.JavaScript,
+    name: "Javascript",
+  },
+  {
+    slug: "ts",
+    color: "blue",
+    description: `2 years experience with Typescript
+
+		Very comfortable using both for writing application code & types as well as library code
+		`,
+    logo: Assets.TypeScript,
+    name: "Typescript",
+  },
+  {
+    slug: "css",
+    color: "blue",
+    description: `3 years experience with CSS
+
+		Very comfortable using with standard stylesheets, inline styles, as well as with TailWindCSS`,
+    logo: Assets.CSS,
+    name: "CSS",
+  },
+  {
+    slug: "html",
+    color: "orange",
+    description: "3 years experience with HTML",
+    logo: Assets.HTML,
+    name: "HTML",
+  },
+  {
+    slug: "tailwind",
+    color: "cyan",
+    description: "1 year experience with Tailwind CSS",
+    logo: Assets.Tailwind,
+    name: "Tailwind CSS",
+  },
+  {
+    slug: "reactjs",
+    color: "cyan",
+    description: `2 Years experience with React.JS
+
+			Very comfortable with functional component style, creating react hooks,
+			managing state / contexts, and server side integrations`,
+    logo: Assets.ReactJs,
+    name: "React Js",
+  },
+  {
+    slug: "expo",
+    color: "gray",
+    description: `1 Year experience with Expo
+
+			Very comfortable scaffolding projects &
+			creating mobile apps using the Expo ecosystem`,
+    logo: Assets.Expo,
+    name: "Expo & React Native",
+  },
+  {
+    slug: "svelte",
+    color: "orange",
+    description: `2 Years experience with Svelte
+
+		Comfortable with the 'Svelte way' of doing things.
+		Used instead of React.js because of it's lower bundle size.
+		`,
+    logo: Assets.Svelte,
+    name: "Svelte",
+  },
+  {
+    slug: "django",
+    color: "lime",
+    description: `2 Years experience with Django
+
+		Very comfortable using to create full stack websites with data storage & authentication.
+		`,
+    logo: Assets.Django,
+    name: "Django",
+  },
+  {
+    slug: "arduino",
+    color: "cyan",
+    description: `2 Years experience with Arduino
+
+		Very comfortable both Programming the Microcontroller using Arduino IDE v2 & creating &
+		creating small electronics.
+		`,
+    logo: Assets.Arduino,
+    name: "Arduino",
+  },
+  {
+    slug: "tensorflow",
+    color: "orange",
+    description: `1 Year experience with Tensorflow
+
+		Very comfortable using to create & train Maching Learning models
+		Have used to create Reinforcement Learning & Supervised Learning
+		`,
+    logo: Assets.Tensorflow,
+    name: "Tensorflow",
+  },
+  {
+    slug: "pytorch",
+    color: "orange",
+    description: `1 Year experience with Pytorch
+
+		Very comfortable using to create & train Maching Learning models
+		Have used to create Reinforcement Learning & Supervised Learning
+		`,
+    logo: Assets.Pytorch,
+    name: "Pytorch",
+  },
+  {
+    slug: "python",
+    color: "yellow",
+    description: `3 Years experience with Python
+
+		Very comfortable using to create small scripts for company utilities &
+		to scaffold entire applications spanning 3000 lines
+
+		Knowledgeable about both the strengths (readability) & weaknesses (speed) of python
+		and when it is most applicable to use over another technology
+		`,
+    logo: Assets.Python,
+    name: "Python",
+  },
+  {
+    slug: "kivy",
+    color: "black",
+    description: `1 Year experience with Kivy
+
+		Very comfortable using to create small python GUI applications
+		& to scaffold GUI's for research projects
+		`,
+    logo: Assets.Kivy,
+    name: "Kivy",
+  },
+  {
+    slug: "java",
+    color: "red",
+    description: `2 Years experience with Java
+
+		Very comfortable using to create enterprise grade applications
+		& to complete programming assignments
+		`,
+    logo: Assets.Java,
+    name: "Java",
+  },
+  {
+    slug: "processing",
+    color: "blue",
+    description: `2 Years experience with Processing
+
+		Very comfortable using to create visual and interactive programs.
+    Have used to create entire GUI's for Java applications & simulate physics.
+		`,
+    logo: Assets.Processing,
+    name: "Processing",
+  },
+  {
+    slug: "p5js",
+    color: "red",
+    description: `2 Years experience with P5.js
+
+		Very comfortable using to create visual and interactive javascript programs.
+    Have used to quickly demonstrate programming ideas like color & transitions.
+		`,
+    logo: Assets.P5JS,
+    name: "P5JS",
+  },
 ];
 
 export default MY_SKILLS;
 
 export const getSkills = (...slugs: Array<string>): Array<Skill> =>
-	MY_SKILLS.filter((it) => slugs.includes(it.slug));
+  MY_SKILLS.filter((it) => slugs.includes(it.slug));
