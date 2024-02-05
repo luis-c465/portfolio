8fdb45e49866b810ee9b32c5027860a7be41ea78
Luis Canada <canadaluis5@gmail.com>
2024-02-05 12:54:12 -0500

    Try deploying

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
-const base = '/slick-portfolio-svelte';
+const base = "/portfolio";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	vitePlugin: {
-		inspector: {
-			showToggleButton: 'always'
-		}
-	},
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  vitePlugin: {
+    inspector: {
+      showToggleButton: "always",
+    },
+  },
 
-	kit: {
-		adapter: adapter({ fallback: '404.html' }),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
-		}
-	}
+  kit: {
+    adapter: adapter({ fallback: "404.html" }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? base : "",
+    },
+  },
 };
 
 export default config;
